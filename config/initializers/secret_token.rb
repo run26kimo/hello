fd9bd9e860571cfea7e2b1effffc9f6e3b465231
run26kimo <run26kimo@gmail.com>
2013-05-22 19:11:49 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '8aaaf1d089db63313322219aadbc389b22bf957dcb0f256b51795ee202826d31ab316ca196749e4edd7dfa6f27e14b655ef86941abb3d2bd9b2b36b368dea46d'
